81de9df4cfed853d2d31539eadb232aa00be28a7
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-08-17 01:23:33 +0200

    init: start 1_lesson

--- src/App.js
+++ src/App.js
@@ -1,25 +1,2 @@
-import logo from './logo.svg';
-import './App.css';
+export const App = () => <h1 className="text-center">Hello World меня зовут: <span className="text-danger">Daniel Kondratiuk</span></h1>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
