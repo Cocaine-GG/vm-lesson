86033651c0ce2dbda5d2116d292fea0c785ff782
Daniel Kondratiuk <venga1.ua@gmail.com>
2021-09-16 18:48:53 +0200

    feat: Lesson 7 in process...

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -4,7 +4,7 @@ import { pagination, usersDataTransform } from '../utils'
 
 import PeopleCounter from './people-counter'
 import GroupList from './group-list'
-import Users from './users'
+import UsersTable from './users-table'
 import Pagination from './pagination'
 
 const App = () => {
@@ -12,6 +12,7 @@ const App = () => {
 	const [professions, setProfessions] = useState(null)
 	const [selectedProf, setSelectedProf] = useState()
 	const [currentPage, setCurrentPage] = useState(1)
+	const [sortBy, setSortBy] = useState({iter: null, order : ''})
 	const handlerUserDelete = (id) =>
 		setUsers(users.filter((user) => user._id !== id))
 	const handlerUserFavorite = (id) =>
@@ -20,27 +21,55 @@ const App = () => {
 				user._id === id ? { ...user, status: !user.status } : { ...user }
 			)
 		)
+	const handlerUsersSort = (item) => {
+		const {order} = sortBy
+		if(order === '') {
+			setSortBy({iter:item, order: 'asc'})
+		}
+		if (order === 'asc'){
+			setSortBy({iter:item, order: 'desc'})
+		}
+		if (order === 'desc'){
+			setSortBy({iter:null, order: ''})
+		}
+	}
 	const handlerProfessionSelect = (item) =>
 		setSelectedProf(item) & setCurrentPage(1)
 	const clearFilter = () => setSelectedProf() & setCurrentPage(1)
+
+	console.log(users)
 	const usersFilter = selectedProf
 		? users.filter(
 			(user) =>
 				JSON.stringify(user.profession) === JSON.stringify(selectedProf)
 		)
 		: users
-	const usersCrop = pagination(usersFilter, currentPage)
-	const usersLength = usersFilter.length
+	const usersSorted = usersFilter.sort((a,b)=>{
+		const {iter, order} = sortBy
+		if(!iter || order === '') return
+		let currentEl = (order === 'asc') ? a : b
+		let nextEl = (order === 'asc') ? b : a
+		if(!isNaN(a[iter]) && !isNaN(b[iter])) return currentEl[iter] - nextEl[iter]
+
+		const [first, second] = iter.split('.')
+		if(!second) {
+			return currentEl[iter].localeCompare(nextEl[iter])
+		}else {
+			return currentEl[first][second].localeCompare(nextEl[first][second])
+		}
+	})
+	const usersCrop = pagination(usersSorted, currentPage)
+	const usersLength = usersSorted.length
 	useEffect(() => {
 		api.professions().then(setProfessions)
 	}, [professions])
 
 	return (
-		<div>
+		<>
 			<PeopleCounter usersLength={usersLength} />
-			<div className="d-flex align-items-start text-dark bg-light">
+			<div className="d-flex align-items-start text-dark bg-light bg-gradient">
 				{professions && (
-					<div className="d-flex flex-column">
+					<div className="d-flex flex-column m-2">
 						<GroupList
 							items={professions}
 							selectedItem={selectedProf}
@@ -51,20 +80,23 @@ const App = () => {
 						</button>
 					</div>
 				)}
-				<div className="w-100">
-					<Users
-						users={usersCrop}
-						onDeleteUser={handlerUserDelete}
-						onFavoriteUser={handlerUserFavorite}
-					/>
-					<Pagination
-						setCurrentPage={setCurrentPage}
-						currentPage={currentPage}
-						itemsCount={usersFilter.length}
-					/>
-				</div>
+				{usersLength > 0 && (
+					<div className="w-100 m-2">
+						<UsersTable
+							users={usersCrop}
+							onDeleteUser={handlerUserDelete}
+							onFavoriteUser={handlerUserFavorite}
+							onSort={handlerUsersSort}
+						/>
+						<Pagination
+							setCurrentPage={setCurrentPage}
+							currentPage={currentPage}
+							itemsCount={usersSorted.length}
+						/>
+					</div>
+				)}
 			</div>
-		</div>
+		</>
 	)
 }
 
--- src/components/user-row.jsx
+++ src/components/user-row.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import FavoriteBtn from './favorite-btn'
 import PropTypes from 'prop-types'
+import Badge from './badge'
 
-const UserRow = ({ user, qualities, onFavoriteUser, onDeleteUser }) => {
+const UserRow = ({ user, onFavoriteUser, onDeleteUser }) => {
 	const { name, profession, completedMeetings, status, rate, _id } = user
 	return (
 		<tr>
 			<td>{name}</td>
-			<td>{qualities}</td>
+			<td>
+				{user.qualities.map((quality) => (
+					<Badge key={user._id + quality.name} quality={quality} />
+				))}
+			</td>
 			<td>{profession.name}</td>
 			<td>{completedMeetings}</td>
 			<td>
@@ -25,7 +30,6 @@ const UserRow = ({ user, qualities, onFavoriteUser, onDeleteUser }) => {
 
 UserRow.propTypes = {
 	user: PropTypes.object.isRequired,
-	qualities: PropTypes.array.isRequired,
 	onFavoriteUser: PropTypes.func.isRequired,
 	onDeleteUser: PropTypes.func.isRequired
 }
